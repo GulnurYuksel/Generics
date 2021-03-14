7e45798e790681dc28d87ef613031d3c3ef11199
Gülnur Yüksel <gulnuryuksel96@gmail.com>
2021-03-14 15:56:04 +0300

    Add project files.

---
+++ Generics/MyDictionary.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Generics
+{
+    class MyDictionary<K,V>
+    {
+        K[] keys;
+        V[] values;
+
+        public MyDictionary()
+        {
+            keys = new K[0];
+            values = new V[0];
+        }
+
+        public void Add(K key, V value)
+        {
+            K[] keyArray = keys;
+            keys = new K[keys.Length + 1];
+            V[] valueArray = values;
+            values = new V[values.Length + 1];
+
+            for (int i = 0; i < keyArray.Length; i++)
+            {
+                keys[i] = keyArray[i];
+                values[i] = valueArray[i];
+            }
+            key = keys[keys.Length - 1];
+            value = values[values.Length - 1];
+        }
+
+        public int Count
+        {
+            get { return keys.Length; }
+        }
+
+    }
+}
---
+++ Generics/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Generics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+            myDictionary.Add(1, "Gülnur Yüksel");
+            myDictionary.Add(2, "Veli Yağmur");
+            myDictionary.Add(3, "Gönül Dursun");
+            myDictionary.Add(4, "Ahmet Yıldız");
+            Console.WriteLine(myDictionary.Count);
+        }
+    }
+}
